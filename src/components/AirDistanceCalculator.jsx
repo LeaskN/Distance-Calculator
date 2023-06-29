03dea498594becc48628adc84692e472f8c43f6e
Nicholas Leask <leask.nicholas@gmail.com>
2023-06-29 16:44:17 -0400

    fix: fixed delay in caluclation

--- src/components/AirDistanceCalculator.jsx
+++ src/components/AirDistanceCalculator.jsx
@@ -1,51 +1,46 @@
 import React, { useState } from "react";
 
 const AirDistanceCalculator = () => {
-  const [pointALatitude, setPointALatitude] = useState('');
-  const [pointALongitude, setPointALongitude] = useState('');
-  const [pointBLatitude, setPointBLatitude] = useState('');
-  const [pointBLongitude, setPointBLongitude] = useState('');
-  const [pointA, setPointA] = useState('');
-  const [pointB, setPointB] = useState('');
+  const initLat1 = 40.8759
+  const initLong1 = -73.5971
+  const initLat2 = 27.7743
+  const initLong2 = -82.6389
+
+  const [latA, setLatA] = useState(initLat1);
+  const [latB, setLatB] = useState(initLat2);
+  const [longA, setLongA] = useState(initLong1);
+  const [longB, setLongB] = useState(initLong2);
   const [distance, setDistance] = useState('');
 
-  const calculateDistance = () => {
-    // Get the longitude and latitude for each point
-    const requestA = new Request(`https://api.opencagedata.com/geocode/v1/json?key=3d07425ad8bd4928944fcdb6457ffde6&q=${pointALatitude}%2C${pointALongitude}`);
-    fetch(requestA)
-    .then((response) => response.json())
-    .then((data) => {
-      const { lng, lat } = data.results[0].geometry;
-      setPointA({ lng, lat });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+
+  var degreeToRad = (deg) => {
+    return deg * (Math.PI/180)
+  }
+
+  var calculateDistance = () => {
+    // convert longitude and latitude from degrees to radians
+    var longRadA = degreeToRad(longA)
+    var longRadB = degreeToRad(longB)
+    var latRadA = degreeToRad(latA)
+    var latRadB = degreeToRad(latB)
+
+    // haversine formula- from https://www.geeksforgeeks.org/program-distance-two-points-earth/
+    var dLong = longRadB - longRadA;
+    var dLat = latRadB - latRadA;
+
+    var a = Math.pow(Math.sin(dLat / 2), 2) 
+            + Math.cos(latRadA) 
+            * Math.cos(latRadB) 
+            * Math.pow(Math.sin(dLong / 2), 2);
     
-    const requestB = new Request(`https://api.opencagedata.com/geocode/v1/json?key=3d07425ad8bd4928944fcdb6457ffde6&q=${pointBLatitude}%2C${pointBLongitude}`);
-    fetch(requestB)
-      .then((response) => response.json())
-      .then((data) => {
-        const { lng, lat } = data.results[0].geometry;
-        setPointB({ lng, lat });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    var c = 2 * Math.asin(Math.sqrt(a));
 
-    // Calculate the distance between the two points
-    const earthRadius = 6371; // In kilometers
-    const dLat = (pointB.lat - pointA.lat) * Math.PI / 180;
-    const dLon = (pointB.lng - pointA.lng) * Math.PI / 180;
-    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(pointA.lat * Math.PI / 180) * Math.cos(pointB.lat * Math.PI / 180) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = earthRadius * c;
-
-    // Set the distance state
-    setDistance(distance);
-  };
+    // Radius of earth in km
+    var R = 6371;
+    // result 
+    var calculatedDist = (c * R).toString()
+    setDistance("Distance: " + calculatedDist + "km");
+  }
 
   return (
     <div style={{ width: 300 }}>
@@ -56,14 +51,14 @@ const AirDistanceCalculator = () => {
           id="latitudeA"
           placeholder="Point A Latitude"
           style={{ width: 100 }}
-          onChange={(event) => setPointALatitude(event.target.value)}
+          onChange={(event) => setLatA(event.target.value)}
         />
         <input
           type="text"
           id="longitudeA"
           placeholder="Point A Longitude"
           style={{ width: 100 }}
-          onChange={(event) => setPointALongitude(event.target.value)}
+          onChange={(event) => setLongA(event.target.value)}
         />
       </div>
       <div>
@@ -72,18 +67,18 @@ const AirDistanceCalculator = () => {
           id="latitudeB"
           placeholder="Point B Latitude"
           style={{ width: 100 }}
-          onChange={(event) => setPointBLatitude(event.target.value)}
+          onChange={(event) => setLatB(event.target.value)}
         />
         <input
           type="text"
           id="longitudeB"
           placeholder="Point B Longitude"
           style={{ width: 100 }}
-          onChange={(event) => setPointBLongitude(event.target.value)}
+          onChange={(event) => setLongB(event.target.value)}
         />
       </div>
       <button onClick={calculateDistance}>Calculate</button>
-      <h3>Distance: { distance } km</h3>
+      <h3>{ distance }</h3>
     </div>
   );
 };
